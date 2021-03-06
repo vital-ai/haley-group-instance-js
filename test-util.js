aa44c91e965dafd1a96d942be49330b74299639b
weijunzhai <weijun.zhai1@gmail.com>
2021-03-06 10:06:59 -0600

    easy unit test setup

--- test-util.js
+++ test-util.js
@@ -2,12 +2,13 @@ global.VITAL_JSON_SCHEMAS = [];
 			
 global.VITAL_LOGGING = true;
 
-global.tv4 = require('./vitalservice/haley/lib-vital/vitalservice-js/tv4.min.js');
+// global.tv4 = require('./vitalservice/haley/lib-vital/vitalservice-js/tv4.min.js');
+global.tv4 = require('@vital-ai/haley/lib-vital/vitalservice-js/tv4.min.js');
 
-global.LRUCache = require('./vitalservice/haley/lib-vital/vitalservice-js/lru.js').LRUCache;
+global.LRUCache = require('@vital-ai/haley/lib-vital/vitalservice-js/lru.js').LRUCache;
 
-require('./vitalservice/haley/lib-vital/vitalservice-js/vital-core-0.2.304.js');
-require('./vitalservice/haley/lib-vital/vitalservice-js/vital-0.2.304.js');
+require('@vital-ai/haley/lib-vital/vitalservice-js/vital-core-0.2.304.js');
+require('@vital-ai/haley/lib-vital/vitalservice-js/vital-0.2.304.js');
 
 // loding vitalservice domains
 var fs = require('fs');
@@ -19,7 +20,7 @@ for(var i = 0 ; i < items.length; i++) {
   require(path.join(__dirname + '/vitalservice/domains', file));
 }
 
-var import1 = require('./vitalservice/haley/lib-vital/vitalservice-js/vitalservice-json-0.2.304.js');
+var import1 = require('@vital-ai/haley/lib-vital/vitalservice-js/vitalservice-json-0.2.304.js');
 const vitaljs = import1.vitaljs;
 global.VitalServiceJson = import1.VitalServiceJson;
 
