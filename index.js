32f6ece72e722eb813e13efe4710dc2cca1f8d00
weijunzhai <weijun.zhai1@gmail.com>
2021-06-26 09:49:34 -0500

    compile web version

--- index.js
+++ index.js
@@ -1725,8 +1725,9 @@ var GeneralGraphContainer = /** @class */ (function (_super) {
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const serverConfig = {
   mode: 'none',
   target: "node",
   entry: './src/index.ts',
@@ -29,3 +29,34 @@ module.exports = {
   },
   externals: [nodeExternals()],
 };
+
+const webConfig = {
+  mode: 'none',
+  target: "web",
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.web.js',
+    path: path.resolve(__dirname, './'),
+    libraryTarget: 'umd',
+    library: { type: 'window' },
+  },
+};
+
+module.exports = [serverConfig, webConfig];
